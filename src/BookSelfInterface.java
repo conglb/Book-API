990f9300cbe7cee5637ba236b2a3792c96780068
Le Ba Cong <conglb@yahoo.com>
2019-02-27 21:48:11 +0700

    Create BookShelfInterface.java

---
+++ src/BookSelfInterface.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface BookSelfInterface extends Remote {
+
+    // Get book with bookId from bookShelf
+    public Book getBook(int bookId) throws RemoteException ;
+    // Insert a book to bookshelf
+    public void insertBook(Book book) throws RemoteException ;
+}
